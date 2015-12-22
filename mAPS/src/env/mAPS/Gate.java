aa75c7ebc9500430f5f211fe1c2b0b6f56bc055f
lcastropg <l.castropg@gmail.com>
2015-12-22 15:34:58 -0200

    Inserting new files
    This commit is just a test. New updates are coming soon

---
+++ mAPS/src/env/mAPS/Gate.java
@@ -0,0 +1,19 @@
+// CArtAgO artifact code for project mAPS
+
+package mAPS;
+
+import cartago.*;
+
+public class Gate extends Artifact {
+	void init(int initialValue) {
+		defineObsProperty("count", initialValue);
+	}
+	
+	@OPERATION
+	void inc() {
+		ObsProperty prop = getObsProperty("count");
+		prop.updateValue(prop.intValue()+1);
+		signal("tick");
+	}
+}
+
